163d4dd33877b71bf2c1281848209725be50660c
trangliverpool3 <trangliverpool3@gmail.com>
2021-06-20 18:21:37 +0700

    add text of utils

--- src/utils/Text.js
+++ src/utils/Text.js
@@ -1,5 +1,76 @@
 // define all of text uses in prj here
 
 export const text = {
+  //Login, Logout,...
+  username: "Username",
+  password: "Mật khẩu",
+  login: "Đăng nhập",
+  changePassword: "Đổi mật khẩu",
+  forgotPassword: "Quên mật khẩu",
+  oldPassword: "Mật khẩu cũ",
+  newPassword: "Mật khẩu mới",
+  repeatPassword: "Xác nhận mật khẩu",
+  errorRepeatPassword: "Mật khẩu và xác nhận mật khẩu không giống nhau",
 
-}
+  //Home
+  kpiByMonth: "KPI theo tháng",
+  salaryByMonth: "Lương theo tháng",
+  averageIncome: "Bình quân thu nhập",
+  subscriberQuality: "Chất lượng thuê bao",
+
+  //KPI theo tháng
+  kpiAchieved: "KPI đạt được", 
+  provisionalSalary: "Lương tạm tính",
+
+
+  //KPI đạt được
+  totalKpi: "KPI tổng",
+  kpiPrepaidSubscribers: "KPI TBTT",
+  kpiPostpaidSubscribers: "KPI TBTS",
+  kpiVas: "KPI VAS",
+
+
+
+  // Lương tạm tính, Lương khoán, Lương theo tháng
+  fixedSalary: "Lương cố định",
+  upSalary: "Lương khoán",
+  prepaidSubscriptionFee: "Phí TBTT",
+  postpaidSSubscriptionFee: "Phí TBTS",
+  vasFee: "Phí VAS",
+  freightCharges: "Phí cước",
+  cardRechargeFee: "Phí thẻ nạp",
+  terminalServiceFee: "Phí thiết bị đầu cuối",
+  orderServiceFee: "Phí dịch vụ khác",
+  numberOfCustomers: "Lượt KH",
+  transactions: "Lượt giao dịch",
+  metricStatus: "Trạng thái số liệu",
+  arrears: "Truy thu",
+
+
+  // Bình quân thu nhập
+  averageMonthlySalary: "Bình quân lương các tháng",
+  averageSalaryInTheFirstQuarter: "Bình quân lương quý 1",
+  fixedAverageSalary: "Bình quân lương cố định",
+  upAverageSalary: "Bình quân lương khoán",
+  averageIncentiveSpending: "Bình quân chi khuyến khích",
+  averageOtherCosts: "Bình quân chi khác", 
+
+
+  // Chất lượng thuê bao
+  numberOfPostpaidSubscribers: "Số lượng TBTS",
+  numberOfCutsAndCancellations: "Số lượng cắt hủy",
+  cancellationRate: "Tỉ lệ cắt hủy",
+  totalRevenue: "Tổng doanh thu",
+  totalDebt90: "Tổng nợ 90",
+  debtRatioRevenue: "Tỉ lệ nợ/ Doanh thu",
+
+
+
+  // Profile
+  profile: "Thông tin cá nhân",
+  workingDay: "Ngày vào làm",
+  workingShop: "Cửa hàng làm việc",
+  traderRating: "Xếp hạng giao dịch viên",
+  storeRating: "Xếp loại cửa hàng",
+
+};
