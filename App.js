976799aa6b7b5cec16340d86418a25296c350c43
Thuan Luu <minhthuan.luu@gmail.com>
2021-06-20 15:15:52 +0700

    Login - Update Pwd - Recover Pwd UI

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignIn from "./src/screens/Auth/SignIn/index";
+import UpdatePassword from './src/screens/Profile/UpdatePassword';
+// import UpdatePassword from "./src/screens/Profile/UpdatePassword/index";
 
 export default function App() {
-  return <SignIn/>
+  return <UpdatePassword/>
 }
 
 const styles = StyleSheet.create({
--- src/screens/Auth/SignIn/index.js
+++ src/screens/Auth/SignIn/index.js
@@ -1,6 +1,5 @@
-// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StatusBar, Text, View, Image } from 'react-native';
+import { SafeAreaView, StatusBar, Text, View, Image, TouchableOpacity } from 'react-native';
 import { Input, Button, AuthTitle, MenuItem } from '../../../comps';
 import { colors } from '../../../utils/Colors';
 import { width } from '../../../utils/Dimenssion';
@@ -11,21 +10,23 @@ const SignIn = (props) => {
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar backgroundColor={colors.primary} />
-            {/* <View style={styles.bottomShape}>
+            <View style={styles.bottomShape}>
                 <Image source={require("../../../assets/loginbg.png")} resizeMode="stretch" style={styles.trigleShape} />
             </View>
-            <View style={{ flex: 1, top: -fontScale(130) }}>
-                <Image source={require("../../../assets/mblogo.png")} resizeMode="contain" style={{ width: width * 2 / 3, alignSelf: "center" }} />
+            <View style={styles.mbfLogoContainer}>
+                <Image source={require("../../../assets/mblogo.png")} resizeMode="contain" style={styles.logo} />
             </View>
             <View style={styles.topShape}>
                 <AuthTitle title="đăng nhập" style={styles.authTitle} />
                 <Input underline title="Username" width={width - fontScale(70)} style={styles.ipUsn} />
                 <Input underline pwd title="Mật khẩu" width={width - fontScale(70)} style={styles.ipPwd} />
-
-                <Button width={fontScale(150)} label={"Đăng nhập"} center style={{ marginTop: fontScale(50) }} />
-            </View> */}
-            <MenuItem value={2000000} style={{marginTop:100}} width={width-50} icon={require("../../../assets/testicon.png")} title="KPI theo tháng" />
-
+                <View style={{ alignSelf: "flex-end", top: 20 }}>
+                    <TouchableOpacity style={styles.forgotTextContainer}>
+                        <Text style={styles.forgotText}>Quên mật khẩu</Text>
+                    </TouchableOpacity>
+                </View>
+                <Button width={fontScale(150)} label={"Đăng nhập"} center style={styles.loginButton} />
+            </View>
         </SafeAreaView>
     );
 }
