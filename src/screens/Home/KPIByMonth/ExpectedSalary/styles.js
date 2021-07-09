a312579171424c40f5cb199c57c7e79334607682
Thuan Luu <minhthuan.luu@gmail.com>
2021-07-09 08:12:50 +0700

    commit to merge Trang's branch

--- src/screens/Home/KPIByMonth/ExpectedSalary/styles.js
+++ src/screens/Home/KPIByMonth/ExpectedSalary/styles.js
@@ -3,25 +3,26 @@ import { colors } from "../../../../utils/Colors";
 import { fontScale } from "../../../../utils/Fonts";
 
 export const styles = StyleSheet.create({
-    container:{ flex: 1,backgroundColor:colors.primary },
-    bodyScr:{ marginTop: fontScale(27) },
-    sumKpiContainer:{ flexDirection: "row",justifyContent:"center" },
-    sumKpiTitle:{ color: colors.black,fontSize:fontScale(21),fontWeight:"bold", marginTop: -fontScale(15) },
-    sumKpi:{ color: colors.lightBlue,fontSize:fontScale(21),marginLeft:fontScale(2),fontWeight:"bold", marginTop: -fontScale(15) },
-   
-    detailInfo:{
-        paddingVertical:fontScale(17),
-        marginHorizontal:fontScale(17),
+    container: { flex: 1, backgroundColor: colors.primary },
+    bodyScr: { marginTop: fontScale(27) },
+    sumKpiContainer: { flexDirection: "row", justifyContent: "center",marginTop:fontScale(5) },
+    sumKpiTitle: { color: colors.black, fontSize: fontScale(21), fontWeight: "bold", marginTop: fontScale(5) },
+    sumKpi: { color: colors.lightBlue, fontSize: fontScale(21), marginLeft: fontScale(2), fontWeight: "bold", marginTop: fontScale(5) },
+
+    detailInfo: {
+        paddingVertical: fontScale(17),
+        marginHorizontal: fontScale(17),
+        marginBottom: fontScale(10),
         shadowColor: "#000",
-        backgroundColor:colors.white,  
-        marginTop:fontScale(40),
-        borderRadius:fontScale(17),
+        backgroundColor: colors.white,
+        marginTop: fontScale(40),
+        borderRadius: fontScale(17),
         shadowOffset: {
             width: 0,
             height: 2,
         },
         shadowOpacity: 0.25,
-        shadowRadius: 3.84,    
+        shadowRadius: 3.84,
         elevation: 5
     }
 })
