e3fe1a802b7d210c11a0532491c384d72124843a
Thuan Luu <minhthuan.luu@gmail.com>
2021-07-13 01:21:08 +0700

    check validate V_Error full api

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import Home from "./Home/Dashboard";
+import Home from "./Home/DashBoard";
 import KPIByMonthDashboard from "./Home/KPIByMonth/DashBoard";
 import Achieve from "./Home/KPIByMonth/Achieve";
 import ExpectedSalary from "./Home/KPIByMonth/ExpectedSalary";
