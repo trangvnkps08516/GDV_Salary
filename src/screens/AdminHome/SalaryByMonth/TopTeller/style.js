16a79f17949038080fbafeeebdd5a358c75506b7
trangvnkps08516 <55426902+trangvnkps08516@users.noreply.github.com>
2021-08-04 23:22:17 +0700

    Delete src/screens/AdminHome/SalaryByMonth/TopTeller directory

--- src/screens/AdminHome/SalaryByMonth/TopTeller/style.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-import { colors } from "../../../../utils/Colors";
-
-export const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:colors.primary
-    }
-})
